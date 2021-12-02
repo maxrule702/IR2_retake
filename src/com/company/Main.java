75060674835cccf55e20b7493b971c4414f659bb
maxrule702 <maximoo2004@gmail.com>
2021-12-02 14:44:48 +0000

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,70 @@
+//package com.company;
+//
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        int current = 0;
+//        int endCheck = 0;
+//        String[] names = {"Ben", "Thor", "Zoe", "Kate"};
+//        System.out.println("which player are you looking for?");
+//        Scanner input = new Scanner(System.in);
+//        String PlayerName = input.next();
+//        int max = names.length;
+//        for (int i = 0; i < max; i++) {
+//            current++;
+//            if (current == 4) {
+//                endCheck++;
+//                break;
+//            }
+//            if (PlayerName.equals(names[current])) {
+//
+//                boolean found = true;
+//                if (found == true) {
+//                    System.out.println("Yes, they have a top score");
+//                }
+//                break;
+//            }
+//        }
+//        if (endCheck == 1) {
+//            System.out.println("No, they do not have a top score");
+//        }
+//    }
+//        }
+//
+//
+//
+//
 package com.company;
-
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-	int pos = 0;
-    int curent = 0;
-    boolean found = false;
-        String [] names = {"Ben","Thor","Zoe","Kate"};
-        System.out.println("which player are you looking for?");
-        Scanner input = new Scanner(System.in);
-        String PlayerName = input.next();
-       int  max = names.length;
-        for (int i = 0; i < max ; i++) {
-            pos++;
-            if (PlayerName.equals(names[pos])){
-            System.out.println("yes they have a top score");
-            curent ++;
-            found = true;
-            break;
-            }
-            if(pos ==3){
-                break;
-            }
-        }
-        if (curent == 0) {
-            System.out.println("they do not have a top score");
-            found = false;
-        }
+        int remainder = 0;
 
+        ArrayList<Integer> binary = new ArrayList<Integer>();
+        System.out.println("please enter your number");
+        Scanner input = new Scanner(System.in);
+        int userNumber = input.nextInt();
+        while (userNumber != 0 ){
+            remainder = userNumber % 2;
+            userNumber = userNumber / 2;
+            binary.add(remainder);
+            System.out.print(userNumber+ " ");
+
+        }
+        int firstPostion = binary.size();
+        System.out.println(firstPostion);
+        int pos = binary.size()-1 ;
+
+        for (int i = 0; i < binary.size();) {
+            System.out.println(binary.get(pos));
+            pos--;
+            if (pos == -1){
+                break;
+                }
+            }
+        }
     }
-}
