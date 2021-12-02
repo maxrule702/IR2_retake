19b85e63d12705d2a5d0f75e691597052b4cc0d8
maxrule702 <maximoo2004@gmail.com>
2021-12-02 16:09:04 +0000

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -36,35 +36,107 @@
 //
 //
 //
+
+
+//package com.company;
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        int remainder = 0;
+//
+//        ArrayList<Integer> binary = new ArrayList<Integer>();
+//        System.out.println("please enter your number");
+//        Scanner input = new Scanner(System.in);
+//        int userNumber = input.nextInt();
+//        while (userNumber != 0 ){
+//            remainder = userNumber % 2;
+//            userNumber = userNumber / 2;
+//            binary.add(remainder);
+//            System.out.print(userNumber+ " ");
+//
+//        }
+//        int firstPostion = binary.size();
+//        System.out.println(firstPostion);
+//        int pos = binary.size()-1 ;
+//
+//        for (int i = 0; i < binary.size();) {
+//            System.out.println(binary.get(pos));
+//            pos--;
+//            if (pos == -1){
+//                break;
+//                }
+//            }
+//        }
+//    }
+
+
 package com.company;
 import java.util.Scanner;
-import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        int remainder = 0;
+        leetsConverter();}
 
-        ArrayList<Integer> binary = new ArrayList<Integer>();
-        System.out.println("please enter your number");
-        Scanner input = new Scanner(System.in);
-        int userNumber = input.nextInt();
-        while (userNumber != 0 ){
-            remainder = userNumber % 2;
-            userNumber = userNumber / 2;
-            binary.add(remainder);
-            System.out.print(userNumber+ " ");
+        public static void leetsConverter() {
+            Scanner input = new Scanner(System.in);
 
-        }
-        int firstPostion = binary.size();
-        System.out.println(firstPostion);
-        int pos = binary.size()-1 ;
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                Scanner console = new Scanner(line);
 
-        for (int i = 0; i < binary.size();) {
-            System.out.println(binary.get(pos));
-            pos--;
-            if (pos == -1){
-                break;
+                while (console.hasNext()) {
+                    String word = console.next();
+                    word = word.toLowerCase();
+                    if (word.contains("a")) {
+                        word = word.replace("a", "4");
+                    }
+                    if (word.contains("b")) {
+                        word = word.replace("b", "6");
+                    }
+                    if (word.contains("e")) {
+                        word = word.replace("e", "3");
+                    }
+                    if (word.contains("i")) {
+                        word = word.replace("i", "1");
+                    }
+                    if (word.contains("l")) {
+                        word = word.replace("l", "1");
+                    }
+                    if (word.contains("m")) {
+                        word = word.replace("m", "(v)");
+                    }
+                    if (word.contains("n")) {
+                        word = word.replace("n", "\\");
+                    }
+                    if (word.contains("o")) {
+                        word = word.replace("o", "0");
+                    }
+                    if (word.contains("s")) {
+                        word = word.replace("s", "5");
+                    }
+                    if (word.contains("t")) {
+                        word = word.replace("t", "7");
+                    }
+                    if (word.contains("v")) {
+                        word = word.replace("v", "\\/");
+                    }
+                    if (word.contains("w")) {
+                        word = word.replace("w", "`//");
+                    }
+                    if (word.charAt(word.length() - 1) == 's') {
+                        word = word.substring(0, word.length() - 1) + "Z";
+                    }
+                    System.out.println(( word +  " "));
                 }
+                }
+
             }
+
+
         }
-    }
+
+
+
